398affed334a9372d664715e19880a37884486fa
Andrés Felipe <fmendez1212@gmail.com>
2020-02-08 14:20:11 -0500

    vec3 class

--- weekend/weekend.cpp
+++ weekend/weekend.cpp
@@ -1,9 +1,8 @@
-// raytracingoneweekend.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <fstream>
 #include <sstream>
+
+#include "vec3.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -11,6 +10,8 @@
 #define STBI_MSC_SECURE_CRT
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+
+
 
 int main()
 {
@@ -25,12 +26,10 @@ int main()
 	{
 		for (size_t i = 0; i < width; ++i)
 		{
-			float r = float(i) / float (width);
-			float g = float(j) / float (height);
-			float b = 0.2f;
-			char ir = char(255.99*r);
-			char ig = char(255.99*g);
-			char ib = char(255.99*b);
+			vec3 col(float(i) / float (width), float(j) / float (height),0.2f);
+			char ir = char(255.99*col[0]);
+			char ig = char(255.99*col[1]);
+			char ib = char(255.99*col[2]);
 			pixels[index++] = ir;
 			pixels[index++] = ig;
 			pixels[index++] = ib;
@@ -42,14 +41,3 @@ int main()
 
 	 return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
