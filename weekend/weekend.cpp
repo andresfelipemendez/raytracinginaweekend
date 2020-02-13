d04759dc147821d4dba0b00c6bb49083822884c4
Andrés Felipe <fmendez1212@gmail.com>
2020-02-12 21:28:27 -0500

    sphere test

--- weekend/weekend.cpp
+++ weekend/weekend.cpp
@@ -12,7 +12,19 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+bool hit_sphere(const vec3& center, float radius, const ray& r) {
+	vec3 oc = r.origin() - center;
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0 * dot(oc, r.direction());
+	float c = dot(oc,oc) - radius*radius;
+	float discriminant = b*b - 4*a*c;
+	return discriminant > 0;
+}
+
 vec3 color(const ray &r){
+	if(hit_sphere(vec3(0,0,-1), 0.5f, r)){
+		return vec3(1,0,0);
+	}
 	vec3 unit_direction = unit_vector(r.direction());
 	float t = 0.5f * (unit_direction.y()+1.0f);
 	return (1.0f - t) * vec3(1.0f, 1.0f, 1.0) + t * vec3(0.5f,0.7f,1.0f);
@@ -20,8 +32,8 @@ vec3 color(const ray &r){
 
 int main()
 {
-	const int width = 400;
-	const int height = 300;
+	const int width = 200;
+	const int height = 100;
 	const int CHANNEL_NUM = 3;
 	uint8_t* pixels = new uint8_t[width * height * CHANNEL_NUM];
 
